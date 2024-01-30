3d6906f8d66ba66789c8ba37a20f8bba008ed98e
WEI JUN <weijunext@gmail.com>
2024-01-30 18:19:13 +0800

    style: update mt

--- components/generator/Generator.tsx
+++ components/generator/Generator.tsx
@@ -83,7 +83,7 @@ const Generator: React.FC = () => {
             {/* left Title */}
             <div className="md:col-span-1">
               <div className="px-4 sm:px-0 sticky top-0">
-                <h3 className="text-lg font-medium leading-6 text-gray-900 dark:text-white">
+                <h3 className="text-lg font-medium leading-6 text-gray-900 dark:text-white mt-5 md:col-span-2 md:mt-0">
                   {generator.title}
                 </h3>
                 <p className="mt-1 text-sm text-gray-600 dark:text-gray-400">
