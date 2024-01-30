9a4df200933a996299392b0718ae29555f65f9bb
WEI JUN <weijunext@gmail.com>
2024-01-30 16:38:38 +0800

    style: update sheet height

--- components/generator/PreviewSheet.tsx
+++ components/generator/PreviewSheet.tsx
@@ -34,19 +34,30 @@ export const PreviewSheet: React.FC<{
           Generate Bio
         </Button>
       </SheetTrigger>
-      <SheetContent className="sm:max-w-lg">
+      <SheetContent className="sm:max-w-lg h-100vh">
         <SheetHeader>
           <SheetTitle>Preview</SheetTitle>
           <SheetDescription>
-            Click &ldquo;Copy code&rdquo; and paste on your github Readme.
+            Click{" "}
+            <span
+              onClick={copyCode}
+              className="underline cursor-pointer text-amber-500 font-bold"
+            >
+              Copy code
+            </span>{" "}
+            and paste on your github Readme.
           </SheetDescription>
         </SheetHeader>
-        <div className="markdown-body border rounded-sm p-2">
+        <div className="markdown-body border rounded-sm p-2 overflow-auto h-[80vh]">
           {htmlParser.parse(githubBio)}
         </div>
         <SheetFooter className="border-t">
-          <Button type="submit" className="mt-2" onClick={copyCode}>
-            <CopyIcon className="h-4 w-4 mr-2" /> Copy code
+          <Button
+            type="submit"
+            onClick={copyCode}
+            className=" select-none rounded-lg bg-amber-500 mt-2 font-sans text-xs font-bold text-white shadow-md shadow-amber-500/20 transition-all hover:shadow-lg hover:shadow-amber-500/50 hover:bg-amber-500 active:opacity-[0.85] disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
+          >
+            <CopyIcon className="h-4 w-4 mr-2" /> Copy Code
           </Button>
         </SheetFooter>
       </SheetContent>
